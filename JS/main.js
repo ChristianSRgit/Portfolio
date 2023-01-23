ab3a3cfdddf2b165f62110e484443dcc1692d74f
christian Wirenet <christian.s.ramundo@gmail.com>
2023-01-23 16:24:17 -0300

    create modules properly

---
+++ JS/main.js
@@ -0,0 +1,67 @@
+
+
+let bannerDashboard = document.getElementById('bannerDashboard');
+let navDir = document.getElementById('navDir');
+let DLMode = document.getElementById('DLMode')
+let body = document.getElementById('body')
+const wrapper = document.getElementById('tiles')
+
+
+
+
+// show/hide dropdown
+bannerDashboard.addEventListener('click',() => {
+    navDir.classList.toggle("hide");
+    DLMode.classList.toggle("hide");
+    bannerDashboard.classList.toggle('fixBorder');
+})
+
+
+document.addEventListener('scroll',bannerMove);
+
+
+
+function bannerMove(){
+    let scrollBefore = 0;
+    let lastScrollPos = 0;
+
+    lastScrollPos = Math.round(window.scrollY)
+    console.log(lastScrollPos)
+    
+    if(lastScrollPos > scrollBefore){
+        console.log('down')
+        scrollBefore = lastScrollPos;
+    }else{
+        console.log('up')
+        scrollBefore = lastScrollPos;
+
+    }
+}
+
+
+
+const createTile = index => {
+    const tile = document.createElement('div');
+
+    tile.classList.add('tile');
+
+    return tile;
+}
+
+const createTiles = quantity => {
+     Array.from(Array(quantity)).map((tile, index) => {
+        wrapper.appendChild(createTile(index));
+     })
+}
+
+
+const createGrid = () => {
+    wrapper.innerHTML = '';
+    let columns = Math.floor(document.body.clientWidth / 50);
+    let rows = Math.floor(document.body.clientHeight / 50);
+
+    createTiles(columns * rows);
+}
+
+createGrid();
+window.onresize = () => createGrid();
--- JS/modulos.js
+++ JS/modulos.js
@@ -1,35 +1,32 @@
 
 
-let bannerDashboard = document.getElementById('bannerDashboard');
-let navDir = document.getElementById('navDir');
-let DLMode = document.getElementById('DLMode')
 let body = document.getElementById('body')
-
-
-bannerDashboard.addEventListener('click',() => {
-    navDir.classList.toggle("hide");
-    DLMode.classList.toggle("hide");
-    bannerDashboard.classList.toggle('fixBorder');
-})
-
-
-document.addEventListener('scroll',bannerMove);
+const wrapper = document.getElementById('tiles')
 
 
 
-function bannerMove(){
-    let scrollBefore = 0;
-    let lastScrollPos = 0;
+const createTile = index => {
+    const tile = document.createElement('div');
 
-    lastScrollPos = Math.round(window.scrollY)
-    console.log(lastScrollPos)
-    
-    if(lastScrollPos > scrollBefore){
-        console.log('down')
-        scrollBefore = lastScrollPos;
-    }else{
-        console.log('up')
-        scrollBefore = lastScrollPos;
+    tile.classList.add('tile');
 
-    }
+    return tile;
 }
+
+const createTiles = quantity => {
+     Array.from(Array(quantity)).map((tile, index) => {
+        wrapper.appendChild(createTile(index));
+     })
+}
+
+
+const createGrid = () => {
+    wrapper.innerHTML = '';
+    let columns = Math.floor(document.body.clientWidth / 50);
+    let rows = Math.floor(document.body.clientHeight / 50);
+
+    createTiles(columns * rows);
+}
+
+createGrid();
+window.onresize = () => createGrid();
