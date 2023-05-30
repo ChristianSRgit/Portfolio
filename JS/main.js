5ebec0ccc576877be5a252b634c9900c223a21f3
Christian Ramundo <Christian.s.ramundo@gmail.com>
2023-05-29 23:25:21 -0300

    Dark and light mode working, details to fix

--- JS/main.js
+++ JS/main.js
@@ -1,6 +1,3 @@
-
-
-let bannerDashboard = document.getElementById('bannerDashboard');
 let navDir = document.getElementById('navDir');
 let DLMode = document.getElementById('DLMode')
 let body = document.getElementById('body')
@@ -9,36 +6,37 @@ let skillsBanner = document.getElementById('skillsBanner')
 
 
 
+let bannerDashboard = document.getElementById('bannerDashboard');
 // show/hide dropdown
-bannerDashboard.addEventListener('click',() => {
-    navDir.classList.toggle("hide");
-    DLMode.classList.toggle("hide");
-    bannerDashboard.classList.toggle('fixBorder');
+bannerDashboard.addEventListener('click', () => {
+  navDir.classList.toggle("hide");
+  DLMode.classList.toggle("hide");
+  bannerDashboard.classList.toggle('fixBorder');
 })
 
 
-document.addEventListener('scroll',bannerMove);
+document.addEventListener('scroll', bannerMove);
 
 
 let scrollBefore = window.scrollY;
 
-function bannerMove(){
-    
+function bannerMove() {
 
-    window.onscroll = function(e){
-        if(scrollBefore < window.scrollY){
-            console.log("down");
-            skillsBanner.classList.remove('movingLeft');
-            skillsBanner.classList.add('movingRight');
 
-        }else{
-            console.log("up")
-            skillsBanner.classList.remove('movingRight');
-            skillsBanner.classList.add('movingLeft');
-        }
-        scrollBefore = window.scrollY;
+  window.onscroll = function (e) {
+    if (scrollBefore < window.scrollY) {
+      console.log("down");
+      skillsBanner.classList.remove('movingLeft');
+      skillsBanner.classList.add('movingRight');
+
+    } else {
+      console.log("up")
+      skillsBanner.classList.remove('movingRight');
+      skillsBanner.classList.add('movingLeft');
     }
-    
+    scrollBefore = window.scrollY;
+  }
+
 }
 
 
@@ -88,9 +86,85 @@ setTimeout(() => {
 
     }, 700);
 
-  }, 700); 
+  }, 700);
 
 }, 600); // initial timeout
 
 console.log("Hey, you made it all the way here! Something caught your attention, right? Well, guess what? We're still working on some amazing things behind the scenes. Stay tuned for up updates!");
 
+//Dark & Light Mode
+
+// Obtén referencias a los inputs
+const LStyleMode = document.getElementById('LStyleMode');
+const DStyleMode = document.getElementById('DStyleMode');
+
+// Agrega eventos para cambiar los fondos
+
+//          LIGHTMODE
+
+LStyleMode.addEventListener('change', function () {
+  if (LStyleMode.checked) {
+
+    // Establecer fondo light mode
+    document.body.classList.add('light-mode');
+    document.body.classList.remove('dark-mode');
+
+    // change all de h tag's color
+    let e = 0;
+    for (e; e <= 5; e++) {
+      const h1Elements = document.getElementsByTagName(`h${e}`);
+      for (let i = 0; i < h1Elements.length; i++) {
+        h1Elements[i].style.color = "#1f1f1f";
+      }
+    }
+    //contact icon
+    const linkedinIcon = document.getElementById('linkedinIcon');
+    const githubIcon = document.getElementById('githubIcon');
+    const gmailIcon = document.getElementById('gmailIcon');
+
+
+    linkedinIcon.setAttribute('fill', '#1f1f1f');
+    githubIcon.setAttribute('fill', '#1f1f1f');
+    gmailIcon.setAttribute('fill', '#1f1f1f');
+  } else {
+    // Establecer fondo por defecto
+    document.body.classList.remove('light-mode');
+  }
+});
+
+
+//          DARKMODE
+
+DStyleMode.addEventListener('change', function () {
+  if (DStyleMode.checked) {
+
+    // Establecer fondo dark mode
+    document.body.classList.add('dark-mode');
+    document.body.classList.remove('light-mode');
+
+    // change all de h tag's color
+    let e = 0;
+    for (e; e <= 5; e++) {
+      const h1Elements = document.getElementsByTagName(`h${e}`);
+      for (let i = 0; i < h1Elements.length; i++) {
+        h1Elements[i].style.color = "#f2e8de";
+      }
+    }
+
+    //contact icon
+    const linkedinIcon = document.getElementById('linkedinIcon');
+    const githubIcon = document.getElementById('githubIcon');
+    const gmailIcon = document.getElementById('gmailIcon');
+
+
+    linkedinIcon.setAttribute('fill', '#f2e8de');
+    githubIcon.setAttribute('fill', '#f2e8de');
+    gmailIcon.setAttribute('fill', '#f2e8de');
+
+
+
+  } else {
+    // defaul background
+    document.body.classList.remove('dark-mode');
+  }
+});
